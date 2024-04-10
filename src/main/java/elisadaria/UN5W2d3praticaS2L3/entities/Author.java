1cf28b1dbed909c8a32bb153cef92b98b8b5b6c6
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-10 16:00:15 +0200

    tables-situationship

--- src/main/java/elisadaria/UN5W2d3praticaS2L3/entities/Author.java
+++ src/main/java/elisadaria/UN5W2d3praticaS2L3/entities/Author.java
@@ -1,19 +1,28 @@
 package elisadaria.UN5W2d3praticaS2L3.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String surname;
     private String email;
     private String dateOfBirth;
     private String avatar;
+    @OneToMany(mappedBy = "author")
+    private List<Blogpost>blogposts;
 }
--- src/main/java/elisadaria/UN5W2d3praticaS2L3/entities/Blogpost.java
+++ src/main/java/elisadaria/UN5W2d3praticaS2L3/entities/Blogpost.java
@@ -1,5 +1,6 @@
 package elisadaria.UN5W2d3praticaS2L3.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +10,18 @@ import lombok.ToString;
 @Setter
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "blogposts")
 public class Blogpost {
+    @Id
+    @GeneratedValue
     private int id;
     private String category;
     private String title;
     private String cover;
     private String content;
     private double readingTime;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
