06c3108d12b02e8fbc3a46cca58f3ba793be223d
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-11 15:05:27 +0200

    Cloudinary-bean

--- src/main/java/elisadaria/UN5W2d3praticaS2L3/payload/BlogpostPayload.java
+++ src/main/java/elisadaria/UN5W2d3praticaS2L3/payload/BlogpostPayload.java
@@ -1,4 +1,4 @@
-package elisadaria.UN5W2d3praticaS2L3.payloads;
+package elisadaria.UN5W2d3praticaS2L3.payload;
 
 public record BlogpostPayload(
          String category,
--- src/main/java/elisadaria/UN5W2d3praticaS2L3/payload/ErrorsPayloadDTO.java
+++ src/main/java/elisadaria/UN5W2d3praticaS2L3/payload/ErrorsPayloadDTO.java
@@ -1,4 +1,4 @@
-package elisadaria.UN5W2d3praticaS2L3.payloads;
+package elisadaria.UN5W2d3praticaS2L3.payload;
 
 import java.time.LocalDateTime;
 
